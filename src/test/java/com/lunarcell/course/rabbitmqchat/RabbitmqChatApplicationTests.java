aa720cfc593515a83cec247df20e259464f5dda3
lunarcell9 <lunarcell9@gmail.com>
2024-03-18 19:37:45 +0900

    first commit

---
+++ src/test/java/com/lunarcell/course/rabbitmqchat/RabbitmqChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunarcell.course.rabbitmqchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
