96c6aa32c39d7761d37c8b86aba37cda5d90a84e
lunarcell9 <lunarcell9@gmail.com>
2024-03-20 15:52:10 +0900

    실습1

--- src/main/java/com/lunarcell/course/rabbitmqchat/RabbitmqChatApplication.java
+++ src/main/java/com/lunarcell/course/rabbitmqchat/RabbitmqChatApplication.java
@@ -1,11 +1,21 @@
 package com.lunarcell.course.rabbitmqchat;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RabbitmqChatApplication {
 
+	@Profile("tutorial")
+	@Bean
+	public CommandLineRunner tutorial() {
+		return new RabbitAmqpTutorialsRunner();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqChatApplication.class, args);
 	}
